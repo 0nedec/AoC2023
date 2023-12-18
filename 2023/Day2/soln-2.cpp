ea79ac183d0c702e8c4011ff6b5222a09ea3cdd1
Carlos <carlosantoniocedeno@gmail.com>
2023-12-18 13:54:01 -0500

    Solved for Day 2 part 2.

---
+++ 2023/Day2/soln-2.cpp
@@ -0,0 +1,132 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <map>
+
+using namespace std;
+
+void printVector(vector<string>& V) {
+	for (const auto& element : V) {
+	cout << element << endl;
+	}
+}
+
+vector<string> splitString(const string& str, const string& pattern) {
+	vector<string> tokens;
+	size_t start = 0;
+	size_t found = str.find(pattern, start);
+
+	while (found != string::npos) {
+		tokens.push_back(str.substr(start, found - start));
+		start = found + pattern.length();
+		found = str.find(pattern, start);
+	}
+	tokens.push_back(str.substr(start));
+	return tokens;
+	
+	
+}
+/*
+bool gameViable(vector<string>& rounds) {
+	int number = 0;
+	string num;
+	map<string, int> max;
+	max["red"] = 12;
+	max["green"] = 13;
+	max["blue"] = 14;
+
+	for (const auto& round : rounds) {
+		for (const auto& color : max) {
+			size_t position = round.find(color.first);
+			while (position != string::npos) {
+				num = round.substr((position-3), 3);
+				number = stoi(num);
+				if (number > color.second) {
+					return(false);
+				}
+				position = round.find(color.first, position + 1);
+			}
+		}
+	}
+	return(true);
+}
+*/
+
+int findGamePower(vector<string>& rounds) {
+	map<string, int> max;
+	max["red"] = 0;
+	max["blue"] = 0;
+	max["green"] = 0;
+	int number = 0, power = 1;
+	string num;
+	
+	for (const auto& round : rounds) {
+		for (const auto& color : max) {
+			size_t position = round.find(color.first);
+			while (position != string::npos) {
+				num = round.substr((position-3), 3);
+				number = stoi(num);
+				if (number > color.second) {
+					max[color.first] = number;
+				
+				}
+				position = round.find(color.first, position+1);	
+			}	
+		}
+	}	
+	for (const auto& color : max) {
+		power *= color.second;
+	} 
+	return power;
+}
+
+int parseLine (string line) {
+	vector<string> splitByColon, splitBySemicolon, splitBySpace;
+	int gamePower = 0, gameNumber = 0;
+	string temptString;
+	
+	splitByColon = splitString(line, ":");
+	splitBySpace = splitString(splitByColon[0], " ");
+	gameNumber = stoi(splitBySpace[1]);
+	splitBySemicolon = splitString(splitByColon[1], ";");
+	
+	/*
+	if (gameViable(splitBySemicolon)){
+		cout << "Game " << gameNumber << " viable."<< endl;
+		return gameNumber;
+	}
+	else {
+		return 0;
+	}
+	*/
+	gamePower = (findGamePower(splitBySemicolon));
+	return(gamePower);
+}
+
+
+
+int main (int argc, char *argv[] ) {
+	
+	if (argc != 2) {
+		cout << "Usage " << argv[0] << "<filename>" << endl;
+		return 1;
+	}
+	
+	ifstream inputfile(argv[1]);
+	string line;
+	int sum = 0;
+		
+	if (!inputfile.is_open()) {
+		cout << "Failed to open input file." << endl;
+	}
+	while(getline(inputfile, line)) {
+		sum += parseLine(line);
+	}
+	
+	cout << sum << endl;
+
+	inputfile.close();
+	return 0;
+}
